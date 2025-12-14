5aa3666abb95d82c39bd96e8739deef70fabd42b
Saket Kothari <saketkothari722@gmail.com>
2025-12-14 10:42:14 +0530

    removed snowpack and used vite

--- setupEditor.js
+++ setupEditor.js
@@ -1,9 +1,13 @@
 // JSON Text Editors
 
-import { EditorState, basicSetup } from "@codemirror/basic-setup"
-import { defaultTabBinding } from "@codemirror/commands"
-import { EditorView, keymap } from "@codemirror/view"
+import { EditorState } from "@codemirror/state"
+import { EditorView, keymap, lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, rectangularSelection, crosshairCursor, highlightActiveLine } from "@codemirror/view"
+import { history, historyKeymap, indentWithTab } from "@codemirror/commands"
 import { json } from "@codemirror/lang-json"
+import { syntaxHighlighting, defaultHighlightStyle, bracketMatching, foldGutter, foldKeymap } from "@codemirror/language"
+import { closeBrackets, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete"
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search"
+import { lintKeymap } from "@codemirror/lint"
 
 // get our actual elements
 export default function setupEditors() {
@@ -11,8 +15,30 @@ export default function setupEditors() {
   const jsonResponseBody = document.querySelector("[data-json-response-body]")
 
   const basicExtensions = [
-    basicSetup,
-    keymap.of([defaultTabBinding]), //allow us tab inside text editor
+    lineNumbers(),
+    highlightActiveLineGutter(),
+    highlightSpecialChars(),
+    history(),
+    foldGutter(),
+    drawSelection(),
+    dropCursor(),
+    EditorState.allowMultipleSelections.of(true),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    bracketMatching(),
+    closeBrackets(),
+    rectangularSelection(),
+    crosshairCursor(),
+    highlightActiveLine(),
+    highlightSelectionMatches(),
+    keymap.of([
+      ...closeBracketsKeymap,
+      indentWithTab,
+      ...historyKeymap,
+      ...foldKeymap,
+      ...completionKeymap,
+      ...searchKeymap,
+      ...lintKeymap,
+    ]),
     json(),
     EditorState.tabSize.of(2),
   ]
